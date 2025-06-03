5c6008ba47e7fb27fb5242833e0fbbb0683e67fd
Gabriel Farias <45922516+gabr14l-f4r14s@users.noreply.github.com>
2025-06-03 16:24:51 -0300

    Delete A3Back 1.2 directory

--- A3Back 1.2/Models/usuarios.js
+++
@@ -1,9 +0,0 @@
-const usuarios = [
-  { nome: 'João Atendente', tipo_funcionario: 'Atendente', user_name: 'joao', senha_usuario: '123', email: 'joao@restaurante.com' },
-  { nome: 'Mario Garçom', tipo_funcionario: 'Garçom', user_name: 'mario', senha_usuario: '123', email: 'mario@restaurante.com' },
-  { nome: 'Maria Gerente', tipo_funcionario: 'Gerente', user_name: 'maria', senha_usuario: '123', email: 'maria@restaurante.com' }
-];
-
-module.exports = usuarios; 
-
-//cópia estática dos Clientes

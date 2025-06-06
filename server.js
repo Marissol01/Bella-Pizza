0c9f4bfa479231c40d67035d1dffaf5cfc13963d
Marissol01 <marissol.orosario@gmail.com>
2025-06-06 17:44:34 -0300

    Alterações feitas: - API de Dashboard feita - Adição de rota para Dashboard - Modificação no server.js para rota de dashboard

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+
 const loginRoute = require('./Routes/loginRoute');
 const indexRoutes = require('./Routes/index');
 const tableRoutes = require('./Routes/tableRoute');
 const reservaRoutes = require('./Routes/Reserve');
+const dashboardRoutes = require('./Routes/dashboardRoute'); 
 
 const port = process.env.PORT || 3000; //para configurar a Variável de Ambiente
-
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public')); // Serve arquivos HTML, CSS, JS
 
-// Revisar a definição das rotas no parãmetro
+// Revisar a definição das rotas no parâmetro
 app.use('/', indexRoutes);
 app.use('/api', loginRoute); 
 app.use('/mesas', tableRoutes);
-app.use('/reservas',reservaRoutes);
-
+app.use('/reservas', reservaRoutes);
+app.use('/dashboard', dashboardRoutes); // <-- Adicione isso!
 
 app.listen(port, () => {
-  console.log('🚀 Servidor rodando em http://localhost:3000');
+  console.log(`🚀 Servidor rodando em http://localhost:${port}`);
 });
